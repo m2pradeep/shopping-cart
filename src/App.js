7ca929ea753ee5710ab858399bd6646a46c63ac3
Pradeep <m2pradeep.s@gmail.com>
2019-12-16 23:33:08 +0530

    updated our app to redirects user home

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, Redirect } from 'react-router-dom';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
@@ -49,8 +49,15 @@ const {setCurrentUser} = this.props;
         <Header/>
         <Switch>
           <Route exact path="/" component={HomePage} />
-          <Route exact path="/shop" component={ShopPage} />
-          <Route exact path="/signin" component={SignInAndSignUpPage} />
+          <Route path="/shop" component={ShopPage} />
+          <Route exact path="/signin" render={()=>
+            this.props.currentUser ? (
+            <Redirect to='/' />
+            ):(
+            <SignInAndSignUpPage/>
+            )
+            }
+            />
         </Switch>
       </div>
     );
@@ -58,8 +65,12 @@ const {setCurrentUser} = this.props;
   
 }
 
+const mapStateToProps = ({user}) => ({
+  currentUser : user.currentUser
+});
+
 const mapDispatchToProps = dispatch => ({
   setCurrentUser: user => dispatch(setCurrentUser(user))
 });
 
-export default connect(null, mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
